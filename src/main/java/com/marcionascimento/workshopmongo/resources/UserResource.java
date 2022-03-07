26e3adce1447663f99165d81ee54995f6cf678df
marcionascimentoweb <mlrnrj2011@hotmailcom>
2022-03-07 18:24:57 -0300

    Atualizacao do usuario com put

--- src/main/java/com/marcionascimento/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marcionascimento/workshopmongo/resources/UserResource.java
@@ -51,6 +51,7 @@ public class UserResource {
 	@RequestMapping(method=RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
 		User obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	} 
@@ -60,5 +61,13 @@ public class UserResource {
 			service.delete(id); 
 			return ResponseEntity.noContent().build();
 		} 
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id ){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	} 
 
 }
