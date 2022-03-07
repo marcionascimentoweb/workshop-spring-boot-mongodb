a3176baf9a9e3cff68e3c26655a154493efd8956
marcionascimentoweb <mlrnrj2011@hotmailcom>
2022-03-07 17:47:56 -0300

    Insercao de usuario com POST

--- src/main/java/com/marcionascimento/workshopmongo/services/UserService.java
+++ src/main/java/com/marcionascimento/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcionascimento.workshopmongo.domain.User;
+import com.marcionascimento.workshopmongo.dto.UserDTO;
 import com.marcionascimento.workshopmongo.repository.UserRepository;
 import com.marcionascimento.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,6 +27,15 @@ public class UserService  {
 			throw new ObjectNotFoundException("Objeto não encontrado");
 		}
 		return user.get();
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		
 	}
 
 }
