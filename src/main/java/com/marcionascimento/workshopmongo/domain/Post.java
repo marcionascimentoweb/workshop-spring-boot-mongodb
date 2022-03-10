39f5f9f64633cbfecedd5a3df350797f7bd388d7
marcionascimentoweb <mlrnrj2011@hotmailcom>
2022-03-10 16:46:28 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/marcionascimento/workshopmongo/domain/Post.java
+++ src/main/java/com/marcionascimento/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.marcionascimento.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.marcionascimento.workshopmongo.dto.AuthorDTO;
+import com.marcionascimento.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,7 +22,10 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
+	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments =  new ArrayList<>();
 	
 	public Post() {
 	}
@@ -73,6 +79,16 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
+	
+	
 
 	@Override
 	public int hashCode() {
@@ -91,7 +107,7 @@ public class Post implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
-	
+
 	
 	
 	
