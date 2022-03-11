f4c553c6dff7c755e6edf3641ca83aae64037796
marcionascimentoweb <mlrnrj2011@hotmailcom>
2022-03-11 16:50:13 -0300

    Consulta simples com @Query

--- src/main/java/com/marcionascimento/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marcionascimento/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.marcionascimento.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.marcionascimento.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{'title': {$tegex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
 	
 	//Consulta pelo titulo
 	List<Post> findByTitleContainingIgnoreCase(String text);
